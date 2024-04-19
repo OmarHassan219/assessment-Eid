767b10952f0ea0ba0ec15f8cde329e7ef1eb020b
OmarHassan219 <omar-hassan219@hotmail.com>
2024-04-19 16:18:01 +0400

    lang edit

--- src/context/AudioContext.jsx
+++ src/context/AudioContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const AudioContext = createContext();
 
 const AppProvider = ({ children }) => {
+    const {i18n} = useTranslation()
   const [pauseAudio, setPauseAudio] = useState(true);
-  const [langu, setlangu] = useState('عربي')
+  const [langu, setlangu] = useState(i18n.language === "en" ? "English" : "عربي");
 
  
 
